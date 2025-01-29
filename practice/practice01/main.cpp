0fb287a43dbaf3082bb22c5b83e5169dc642928c
NicholasSeward <nicholas.seward@gmail.com>
2025-01-29 11:55:29 -0600

    we added practice01 and got this all structured for one solution

--- practice/practice01/main.cpp
+++ practice/practice01/main.cpp
@@ -1,4 +1,14 @@
-asdf
-adg
-asdf
-asdf
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> nums;
+    nums.push_back(34);
+    nums.push_back(34);
+    nums.push_back(45);
+    nums.push_back(87);
+    nums.push_back(rand());
+    std::cout << nums.size() << "Hello World!\n";
+}
